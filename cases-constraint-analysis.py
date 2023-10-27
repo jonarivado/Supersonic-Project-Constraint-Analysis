fa3d23f6a846cf41518d5ae8be3377436ebf1d69
kheidbrink <kheidbrink@ethz.ch>
2023-10-27 17:34:16 +0200

    implemented simplified master equations, new plots

--- cases-constraint-analysis.py
+++ cases-constraint-analysis.py
@@ -35,18 +35,30 @@ alpha = 1  # thrust lapse factor
 beta = 1  # how much the weight of the aircraft is reduced by the fuel burn compared to MTOW
 
 # Case 1: Constant speed cruise at constant altitude (P_s = 0)
+# simplified master equation
+def TSL_WTO_M1(WTO_S):
+    return (beta / alpha) * (k * (beta / q) * WTO_S + k2 + (CD0 + CDR) / ((beta / q) * WTO_S))
+# Optimum points
 # Minimum Weight to Area ratio WTO_S_1 (Wing loading)
 WTO_S_1 = (q / beta) * (np.sqrt((CD0 + CDR) / k))
 # Minimum Thrust to Weight ratio TSL_WTO_1 (Thrust loading)
 TSL_WTO_1 = (beta / alpha) * (2 * np.sqrt(k * (CD0 + CDR)) + k2)
 
 # Case 2: Constant speed climb (kinetic part of P_s = 0)
+# simplified master equation
+def TSL_WTO_M2(WTO_S):
+    return (beta / alpha) * (k * (beta / q) * WTO_S + k2 + (CD0 + CDR) / ((beta / q) * WTO_S) + (1 / V) * h_climb)
+# Optimum points
 # Wing loading
 WTO_S_2 = (q / beta) * (np.sqrt((CD0 + CDR) / k))
 # Thrust loading
 TSL_WTO_2 = (beta / alpha) * (2 * np.sqrt(k * (CD0 + CDR)) + k2 + (1 / V) * h_climb)
 
 # Case 3: Constant altitude, speed turn (P_s = 0)
+# simplified master equation
+def TSL_WTO_M3(WTO_S):
+    return (beta / alpha) * (k * n**2 * (beta / q) * WTO_S + k2 * n + (CD0 + CDR) / ((beta / q) * WTO_S) + (1 / V) * h_climb)
+# Optimum points
 # Wing loading
 WTO_S_3 = (q / beta * n) * (np.sqrt((CD0 + CDR) / k))
 # Thrust loading
@@ -54,10 +66,19 @@ TSL_WTO_3 = (beta * n / alpha) * (2 * np.sqrt(k * (CD0 + CDR)) + k2 + (1 / V) * 
 
 # Case 4: Service Ceiling (max. altitude performance)
 # same as for constant speed climb
+# simplified master equation
+def TSL_WTO_M4(WTO_S):
+    return (beta / alpha) * (k * (beta / q) * WTO_S + k2 + (CD0 + CDR) / ((beta / q) * WTO_S) + (1 / V) * h_climb)
 
-# Case 5: Constant altitude, constant acceleration
+# Case 5: Constant altitude, horizontal acceleration
+# simplified master equation
+# TODO implement
 
 # Case 6: Climb angle
+# simplified master equation
+def TSL_WTO_M6(WTO_S):
+    return (beta / alpha) * (k * (beta / q) * WTO_S + k2 + (CD0 + CDR) / ((beta / q) * WTO_S) + np.sin(theta))
+# Optimum points
 # Wing loading
 WTO_S_6 = (q / beta) * (np.sqrt((CD0 + CDR) / k))
 # Thrust loading
@@ -75,14 +96,29 @@ print(df)
 wing_loading = [WTO_S_1, WTO_S_2, WTO_S_3, WTO_S_2, WTO_S_2, WTO_S_6]
 thrust_loading = [TSL_WTO_1, TSL_WTO_2, TSL_WTO_3, TSL_WTO_2, TSL_WTO_2, TSL_WTO_6]
 
-plt.title('Plot Constraint Analysis')
+# plt.axis((950, 1000, -0.5, 15))
+WTO_S = np.arange(100, 1000, 10)
+# plot with only simplified master equations
+"""
+plt.grid(True)
 plt.xlabel('W/S - Wing Loading [N/m^2]')
 plt.ylabel('T/W - Thrust Loading [-]')
-plt.grid(True)
-plt.axis((950, 1000, -0.5, 15))
-case_index = 0
+plt.plot(WTO_S, TSL_WTO_M1(WTO_S=WTO_S), '-bo', WTO_S, TSL_WTO_M2(WTO_S=WTO_S), '-ro', WTO_S, TSL_WTO_M3(WTO_S=WTO_S), '-go', WTO_S, TSL_WTO_M4(WTO_S=WTO_S), '-mo', WTO_S, TSL_WTO_M6(WTO_S=WTO_S), '-ko')
+plt.show()
+"""
+# subplots with simplified master equations and optimum points
+fig, axs = plt.subplots(2)
+fig.suptitle('Constraint Plots and optimal points')
+axs[0].grid(True)
+axs[1].grid(True)
+axs[0].set_xlabel('W/S - Wing Loading [N/m^2]')
+axs[0].set_ylabel('T/W - Thrust Loading [-]')
+axs[0].plot(WTO_S, TSL_WTO_M1(WTO_S=WTO_S), '-bo', WTO_S, TSL_WTO_M2(WTO_S=WTO_S), '-ro', WTO_S, TSL_WTO_M3(WTO_S=WTO_S), '-go', WTO_S, TSL_WTO_M4(WTO_S=WTO_S), '-mo', WTO_S, TSL_WTO_M6(WTO_S=WTO_S), '-ko')
+axs[1].plot(wing_loading, thrust_loading, 'c*')
+plt.show()
+
+# use for naming of points
+"""case_index = 0
 for t, i in zip(wing_loading, thrust_loading):
     plt.annotate(str(case_index), (t, i), xytext=(t+1, i+1))
-    case_index += 1
-plt.plot(wing_loading, thrust_loading, 'ro')
-plt.show()
+    case_index += 1"""
