971d0876b2cb685dd855823fe81e31d7132f4567
Jonathan Dotzler <jonathan.dotzler@gmail.com>
2023-11-15 12:13:01 +0100

    Fixed error in plot title

--- supersonic_analysis.py
+++ supersonic_analysis.py
@@ -90,5 +90,5 @@ class ConstraintAnalysis:
         plt.xlim(0, 500)
         plt.ylim(0, 2)
         plt.legend()
-        plt.title('Optima w/ margin: T/W: ' +str(round(self.res.x[1],3))+ ', W/S: '+str(round(self.res.x[0],3)))
+        plt.title('Optima w/ margin: T/W: ' +str(round(self.res.x[0],3))+ ', W/S: '+str(round(self.res.x[1],3)))
         plt.show()
